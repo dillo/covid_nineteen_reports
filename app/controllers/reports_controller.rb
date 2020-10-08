d27f59361e34f2830d5dc5d566c44bea1ff6b7b9
Dillo Raju <dilloshion@gmail.com>
2020-10-08 08:26:13 -0400

    WIP

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,15 @@
 class ReportsController < ApplicationController
   def create
     pandemic = Pandemic.find(reports_params[:pandemic_id])
-    CreateReportEventJob.perform_later(pandemic)
 
-    redirect_to pandemic_url(pandemic)
-    flash[:notice] = "Report for #{pandemic.name} is being generated and will be available shortly."
+    begin
+      CreateReportEventJob.perform_later(pandemic)
+    rescue Redis::ConnectionError, Redis::CannotConnectError => e
+      logger.error "REDIS::ERROR::#{e.message}"
+    ensure
+      redirect_to pandemic_url(pandemic)
+      flash[:notice] = "Report for #{pandemic.name} is being generated and will be available shortly."
+    end
   end
 
   private
--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,7 +1,5 @@
 class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  retry_on ActiveRecord::Deadlocked, wait: 5.seconds, attempts: 3
+  retry_on Net::OpenTimeout, Timeout::Error, wait: :exponentially_longer, attempts: 10
+  discard_on ActiveJob::DeserializationError
 end
